cf7bf46882bb3b319a832fdacab74c4ce57f1c00
tlee9 <tuanjung@mx-star.com>
2023-05-17 16:13:29 +0800

    first commit

--- php/conn.php
+++ php/conn.php
@@ -25,9 +25,9 @@ use PDOException;
 use RuntimeException;
 use TypeError;
 $username = getenv('root'); // e.g. 'your_db_user'
-            $password = getenv('qawsed22'); // e.g. 'your_db_password'
-            $dbName = getenv('testdb'); // e.g. 'your_db_name'
-            $instanceUnixSocket = getenv('/cloudsql/extreme-arch-384104:asia-east1:coffee-test'); // e.g. '/cloudsql/project:region:instance'
+$password = getenv('qawsed22'); // e.g. 'your_db_password'
+$dbName = getenv('testdb'); // e.g. 'your_db_name'
+$instanceUnixSocket = getenv('/cloudsql/extreme-arch-384104:asia-east1:coffee-test'); // e.g. '/cloudsql/project:region:instance'
 
             // Connect using UNIX sockets
             $dsn = sprintf(
@@ -42,44 +42,4 @@ $username = getenv('root'); // e.g. 'your_db_user'
                 $username,
                 $password,
             );
-class DatabaseUnix
-{
-    public static function initUnixDatabaseConnection(): PDO
-    {
-        try {
-            // Note: Saving credentials in environment variables is convenient, but not
-            // secure - consider a more secure solution such as
-            // Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
-            // keep secrets safe.
-            
-        } catch (TypeError $e) {
-            throw new RuntimeException(
-                sprintf(
-                    'Invalid or missing configuration! Make sure you have set ' .
-                        '$username, $password, $dbName, ' .
-                        'and $instanceUnixSocket (for UNIX socket mode). ' .
-                        'The PHP error was %s',
-                    $e->getMessage()
-                ),
-                (int) $e->getCode(),
-                $e
-            );
-        } catch (PDOException $e) {
-            throw new RuntimeException(
-                sprintf(
-                    'Could not connect to the Cloud SQL Database. Check that ' .
-                        'your username and password are correct, that the Cloud SQL ' .
-                        'proxy is running, and that the database exists and is ready ' .
-                        'for use. For more assistance, refer to %s. The PDO error was %s',
-                    'https://cloud.google.com/sql/docs/mysql/connect-external-app',
-                    $e->getMessage()
-                ),
-                (int) $e->getCode(),
-                $e
-            );
-        }
-
-        return $conn;
-    }
-}
-# [END cloud_sql_mysql_pdo_connect_unix]
+?>
--- php/insert.php
+++ php/insert.php
@@ -1,5 +1,5 @@
 <?php
-include_once('conn.php');
+require_once('conn.php');
 
 $name= $_POST['name'];
 $phone=$_POST['phone'];
