3e48a53648afb7de845a91957d58f3a8f2c33681
tlee9 <tuanjung@mx-star.com>
2023-05-17 16:51:22 +0800

    first commit

--- php/conn.php
+++ php/conn.php
@@ -6,15 +6,7 @@ declare(strict_types=1);
 namespace Google\Cloud\Samples\CloudSQL\MySQL;
 
 use PDO;
-use PDOException;
-use RuntimeException;
-use TypeError;
 
-class DatabaseUnix
-{
-    public static function initUnixDatabaseConnection(): PDO
-    {
-        try {
             // Note: Saving credentials in environment variables is convenient, but not
             // secure - consider a more secure solution such as
             // Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
@@ -45,39 +37,6 @@ class DatabaseUnix
                 ]
                 # [END_EXCLUDE]
             );
-        } catch (TypeError $e) {
-            throw new RuntimeException(
-                sprintf(
-                    'Invalid or missing configuration! Make sure you have set ' .
-                        '$username, $password, $dbName, ' .
-                        'and $instanceUnixSocket (for UNIX socket mode). ' .
-                        'The PHP error was %s',
-                    $e->getMessage()
-                ),
-                (int) $e->getCode(),
-                $e
-            );
-        } catch (PDOException $e) {
-            throw new RuntimeException(
-                sprintf(
-                    'Could not connect to the Cloud SQL Database. Check that ' .
-                        'your username and password are correct, that the Cloud SQL ' .
-                        'proxy is running, and that the database exists and is ready ' .
-                        'for use. For more assistance, refer to %s. The PDO error was %s',
-                    'https://cloud.google.com/sql/docs/mysql/connect-external-app',
-                    $e->getMessage()
-                ),
-                (int) $e->getCode(),
-                $e
-            );
-        }
-
-        return $conn;
-    }
-};
-# [END cloud_sql_mysql_pdo_connect_unix]
-$obj = new DatabaseUnix();
-$obj1= $obj->initUnixDatabaseConnection();
-$result=$obj1->query("select * from customer");
+$result=$conn->query("select * from customer");
 echo $result;
 ?>
