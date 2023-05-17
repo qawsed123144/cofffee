82167905152687dfb72afb69cde2efc70aed4d28
tlee9 <tuanjung@mx-star.com>
2023-05-17 16:17:04 +0800

    first commit

--- php/conn.php
+++ php/conn.php
@@ -1,19 +1,4 @@
 <?php
-/*
- * Copyright 2020 Google LLC.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 declare(strict_types=1);
 
@@ -27,17 +12,17 @@ use TypeError;
 $username = getenv('root'); // e.g. 'your_db_user'
 $password = getenv('qawsed22'); // e.g. 'your_db_password'
 $dbName = getenv('testdb'); // e.g. 'your_db_name'
-$instanceUnixSocket = getenv('/cloudsql/extreme-arch-384104:asia-east1:coffee-test'); // e.g. '/cloudsql/project:region:instance'
+$instanceUnixSocket = getenv('/cloudsql/extreme-arch-384104:asia-east1:coffee-test'); 
+// e.g. '/cloudsql/project:region:instance'
 
-            // Connect using UNIX sockets
-            $dsn = sprintf(
+$dsn = sprintf(
                 'mysql:dbname=%s;unix_socket=%s',
                 $dbName,
                 $instanceUnixSocket
             );
 
             // Connect to the database.
-            $conn = new PDO(
+$conn = new PDO(
                 $dsn,
                 $username,
                 $password,
