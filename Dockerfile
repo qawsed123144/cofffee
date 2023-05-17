78f16c96d91ba3c7876e03e7bbab6aac37144204
tlee9 <tuanjung@mx-star.com>
2023-05-17 14:50:04 +0800

    first commit

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,4 @@
 FROM php:8.0-apache
-RUN docker-php-ext-install mysqli
-RUN docker-php-ext-enable mysqli
-RUN apachectl restart
 
 EXPOSE 80
 COPY php .
--- php/conn.php
+++ php/conn.php
@@ -1,10 +1,28 @@
 <?php
-$server='34.81.211.175';
 $username='root';
 $password='qawsed22';
 $dbname='testdb';
+$instanceUnixSocket = getenv('extreme-arch-384104:asia-east1:coffee-test');
 
-$conn=new mysqli($server,$username,$password,$dbname);
+$dsn = sprintf(
+    'mysql:dbname=%s;unix_socket=%s',
+    $dbname,
+    $instanceUnixSocket
+);
+
+$conn = new PDO(
+    $dsn,
+    $username,
+    $password,
+    # [START_EXCLUDE]
+    // Here we set the connection timeout to five seconds and ask PDO to
+    // throw an exception if any errors occur.
+    [
+        PDO::ATTR_TIMEOUT => 5,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    ]
+    # [END_EXCLUDE]
+);
 
 // if(!empty($conn->connect_error)){
 //     die('資料庫連線錯誤:' . $conn->connect_error);
