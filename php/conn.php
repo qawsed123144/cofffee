5f5a43623f3af1ef26b5d24c6c0be7a79bf4bf32
tlee9 <tuanjung@mx-star.com>
2023-05-17 13:03:21 +0800

    first commit

--- php/conn.php
+++ php/conn.php
@@ -1,4 +1,23 @@
 <?php
+/*
+ * Copyright 2020 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types=1);
+
+# [START cloud_sql_mysql_pdo_connect_tcp]
 namespace Google\Cloud\Samples\CloudSQL\MySQL;
 
 use PDO;
@@ -17,7 +36,7 @@ class DatabaseTcp
             // keep secrets safe.
             $username = getenv('root'); // e.g. 'your_db_user'
             $password = getenv('qawsed22'); // e.g. 'your_db_password'
-            $dbName = getenv('testdb'); // e.g. 'your_db_name'
+            $dbName = getenv('testdb); // e.g. 'your_db_name'
             $instanceHost = getenv('34.81.211.175'); // e.g. '127.0.0.1' ('172.17.0.1' for GAE Flex)
 
             // Connect using TCP
@@ -28,7 +47,16 @@ class DatabaseTcp
                 $dsn,
                 $username,
                 $password,
-                # ...
+                # [START_EXCLUDE]
+                # [START cloud_sql_mysql_pdo_timeout]
+                // Here we set the connection timeout to five seconds and ask PDO to
+                // throw an exception if any errors occur.
+                [
+                    PDO::ATTR_TIMEOUT => 5,
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                ]
+                # [END cloud_sql_mysql_pdo_timeout]
+                # [END_EXCLUDE]
             );
         } catch (TypeError $e) {
             throw new RuntimeException(
@@ -59,5 +87,4 @@ class DatabaseTcp
         return $conn;
     }
 }
-
-?>
+# [END cloud_sql_mysql_pdo_connect_tcp]
